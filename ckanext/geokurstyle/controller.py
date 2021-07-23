20c85c0634afe9f963a3dbc120c1326b9396cfe1
rue_a <arne.ruemmler@gmail.com>
2021-07-23 13:37:58 +0200

    add quality register

---
+++ ckanext/geokurstyle/controller.py
@@ -0,0 +1,13 @@
+import ckan.plugins as p
+import ckan.lib.helpers as h
+
+import ckan.lib.base as base
+
+
+class GeokurstyleController(base.BaseController):
+
+    def add_metric(self):
+        return base.render('geokurstyle/add_metric.html')
+    def quality_register(self):
+        return base.render('geokurstyle/quality_register.html')
+
--- ckanext/geokurstyle/plugin.py
+++ ckanext/geokurstyle/plugin.py
@@ -4,10 +4,52 @@ import ckan.plugins.toolkit as toolkit
 
 class GeokurstylePlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
+    plugins.implements(plugins.IDatasetForm)
+    plugins.implements(plugins.IValidators)
+    plugins.implements(plugins.ITemplateHelpers)
+    plugins.implements(plugins.IRoutes)
 
     # IConfigurer
 
-    def update_config(self, config_):
-        toolkit.add_template_directory(config_, 'templates')
-        toolkit.add_public_directory(config_, 'public')
+    def get_validators(self):
+        return {
+        }
+
+    def get_helpers(self):
+        return {
+        }
+
+    def is_fallback(self):
+        # Return True to register this plugin as the default handler for
+        # package types not handled by any other IDatasetForm plugin.
+        return True
+
+    def package_types(self):
+        # This plugin doesn't handle any special package types, it just
+        # registers itself as the default (above).
+        return []
+
+    def update_config(self, config):
+        # Add this plugin's templates dir to CKAN's extra_template_paths, so
+        # that CKAN will use this plugin's custom templates.
+        toolkit.add_template_directory(config, 'templates')
+        toolkit.add_public_directory(config, 'public')
+        toolkit.add_resource('assets', 'ckanext-geokurstyle')
         toolkit.add_resource('public', 'ckanext-geokurstyle')
+
+
+    def before_map(self, map):
+
+        return map
+
+    def after_map(self, map):
+        map.connect('add_metric', '/add-metric',
+                    controller='ckanext.geokurstyle.controller:GeokurstyleController',
+                    action='add_metric')
+        # map.connect('edit_process', '/edit-process',
+        #             controller='ckanext.geokurstyle.controller:GeokurstyleController',
+        #             action='edit_process')
+        map.connect('quality_register', '/quality-register',
+                    controller='ckanext.geokurstyle.controller:GeokurstyleController',
+                    action='quality_register')
+        return map
